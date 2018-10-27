79daf0b536c914ca95b23bdfb3dc9858884f2832
Arvin Lin <arvin_lin@my.cuesta.edu>
2018-10-26 23:17:27 -0700

    Add rocket image to Billboard

--- src/component/home/Billboard.js
+++ src/component/home/Billboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import rocket from '../../rocket.png';
+
 import '../../style/Billboard.css';
 
 class Billboard extends React.Component
@@ -8,7 +10,10 @@ class Billboard extends React.Component
     {
         return (
             <div className='billboard'>
-                
+                <div className='rocketLauncher'>
+                    <img className='rocket' src={rocket} />
+                </div>
+                <h1>Better Than Merely.</h1>
             </div>
         );
     }
